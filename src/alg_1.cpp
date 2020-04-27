0b5bb0ddf8e42b6f0863bbf5e87fe1766f741929
SalamWord <62811867+SalamWord@users.noreply.github.com>
2020-04-27 14:30:16 +0300

    Create alg_1.cpp

---
+++ src/alg_1.cpp
@@ -0,0 +1,131 @@
+#include "tstack.h"
+#include <iostream>
+#include <string>
+
+TStack<char>stack1;
+TStack<int>stack2;
+
+int prior(char a)
+{
+	int mas[] = { 0,1,2,2,3,3};
+	char str[] = "()+-*/";
+	int tmp=strchr(str, a)-str;
+	return mas[tmp];
+}
+
+std::string infx2pstfx(std::string inf)
+{
+	int k = 0, k1 = 0,k2=0;
+	std:: string tmp;
+	for (int i = 0; i < inf.size(); i++)
+	{
+		if (!(inf[i] >= '0' && inf[i] <= '9'))
+		{
+			
+			if (stack1.isEmpty() == true)
+			{
+				stack1.push(inf[i]);
+				continue;
+			}
+			k = prior(inf[i]);
+			if (k == 0)
+			{
+				stack1.push(inf[i]);
+				continue;
+			}
+			k1 = prior(stack1.get());
+			if (k >k1)
+				stack1.push(inf[i]);
+			else 
+			{
+				if (inf[i] == ')')
+				{
+					char sym = stack1.get();
+					while (prior(sym)>=k)
+					{
+							tmp += sym;
+							stack1.pop();
+							sym= stack1.get();
+					}
+					stack1.pop();
+				}
+				else
+				{
+					while (stack1.isEmpty() == false)
+					{ 
+						if (!(stack1.get() == '(' || stack1.get() == ')'))
+						{
+							k2 = prior(stack1.get());
+							if (k2 >= k)
+							{
+								tmp += stack1.get();
+								stack1.pop();
+							}
+							else
+								stack1.pop();
+						}
+						else
+							break;
+					}
+					while (stack1.get() == '(' || stack1.get() == ')')
+						stack1.pop();
+					stack1.push(inf[i]);
+				}
+
+			}
+		}
+		else
+			tmp+= inf[i];
+	}
+	while (stack1.isEmpty() == false)
+	{
+		
+		tmp += stack1.get();
+		stack1.pop();
+	}
+	return tmp;
+}
+
+int ex(int k1, int k2, char pst)
+{
+	switch (pst)
+	{
+	case '+':
+		return k1 + k2;
+	case '-':
+	{
+		if (k2 > k1)
+			return  k2 - k1;
+		else  return k1 - k2;
+	}
+	case '*':
+	return k2* k1;
+	case '/':
+	{
+		if (k2 > k1)
+			return k2 / k1;
+		else return k1 / k2;
+	}
+	}
+}
+int eval(std::string pst)
+{
+	std::string tmp;
+	int res;
+	for (int i = 0; i < pst.size(); i++)
+	{
+		
+		if(pst[i]>='0' && pst[i]<='9')
+			stack2.push(pst[i] - '0');
+		else
+		{
+			int k1 = stack2.get();
+			stack2.pop();
+			int k2 = stack2.get();
+			stack2.pop();
+			res = ex(k1, k2, pst[i]);
+			stack2.push(res);
+		}
+	}
+	return stack2.get();
+}
