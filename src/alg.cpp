1fed4ffd0fbfe2939a444100cbd473add540abba
ksidyagin <64638744+ksidyagin@users.noreply.github.com>
2020-05-02 12:19:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,18 +1,15 @@
+#include <iostream>
 #include "tstack.h"
 #include <string>
-
 std::string infx2pstfx(std::string inf)
 {
-TStack<char> stack1;
-    
+TStack<char> stack1;    
     std::string res;
-  
     int len = inf.length();
     for (int i = 0; i < len; i++) {
         if ('0' <= inf[i] && inf[i] <= '9') { res += inf[i]; }
         else if (inf[i] == '(')
             stack1.push(inf[i]);
-
  else if ((inf[i] == '+' || inf[i] == '-') && (stack1.get() != '*') && (stack1.get() != '/') && (stack1.get() != '+') && (stack1.get() != '-'))
             stack1.push(inf[i]);
         else if ((inf[i] == '*') || (inf[i] == '/') && ((stack1.get() != '*') || (stack1.get() != '/')))
@@ -25,7 +22,6 @@ TStack<char> stack1;
             }
             stack1.pop();
         }
-
         else
         {
             if (inf[i] == '+' || inf[i] == '-')
@@ -42,10 +38,8 @@ TStack<char> stack1;
                     stack1.pop();
                }
                 stack1.push(inf[i]);
-         }
-          
-        }
-      
+         }         
+        }     
     }
     while (!stack1.isEmpty()) {
         res += stack1.get();
@@ -54,16 +48,13 @@ TStack<char> stack1;
     inf = res;
     return inf;
 }
-
 int eval(std::string pst)
 {
 TStack<int> stack2;
-
     int len = pst.length();
     for (int i = 0; i < len; i++) {
         if ('0' <= pst[i] && pst[i] <= '9') {    
           int x = pst[i] - '0';
-
             stack2.push(x);
         }
         if (pst[i] == '+') {
